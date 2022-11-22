19f5a6caf22c86d6f749b445adccde29a30694cd
qweri0p <71005650+qweri0p@users.noreply.github.com>
2022-11-22 19:25:31 +0100

    TESCOOOOOOO!!

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  let count = 0;
+  function addtesco() {
+    count++
+  }
+</script>
+
+<button on:click={addtesco}>{count} 💀 {count ===1 ? 'Tesco': 'Tescos'} 💀</button>
+
+<style>
+  button {
+    transition: all 100ms ease-in-out;
+    font-size: 10vh;
+    margin: 250px
+  }
+  button:hover {
+    transform: scale(1.1);
+  }
+  button:active {
+    animation: clicked 200ms linear;
+  }
+  @keyframes clicked {
+    0% {
+      transform: scale(1);
+    }
+    50% {
+      transform: scale(0.69);
+    }
+    100% {
+      transform: scale(1);
+      rotate: 360deg;
+    }
+  }
+</style>
